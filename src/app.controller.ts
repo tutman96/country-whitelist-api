78a66f0e5a6eac2eacc98eda7ce91fc386b57c9f
Brandon Tuttle <11356668+tutman96@users.noreply.github.com>
2020-05-28 19:47:14 -0400

    Add more API documentation to the APIs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Get, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ValidationRequest, ValidationResponse } from './app.dto';
-import { STATUS_CODES } from 'http';
-import { ApiResponse, ApiOkResponse } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiBadRequestResponse, ApiNotFoundResponse } from '@nestjs/swagger';
 
 @Controller('/validate-ip')
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Post()
+  @ApiOperation({
+    summary: 'Validate the location of an IP address',
+    description: 'Given an IP address and a whitelist of countries, return whether or not that IP address is registered in one of those countries'
+  })
   @HttpCode(HttpStatus.OK)
   @ApiOkResponse({ type: ValidationResponse })
+  @ApiBadRequestResponse({ description: 'There was no IP address information found for that IP address' })
+  @ApiNotFoundResponse({ description: 'The IP address provided doesn\'t resolve to any country' })
+  @Post()
   async validateIPAddress(@Body() validationRequest: ValidationRequest) {
     return await this.appService.validateIPAddressInOneOfCountry(
       validationRequest.ipAddress,
--- src/health.controller.ts
+++ src/health.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('/healthz')
 export class HealthController {
+	@ApiOperation({
+		summary: 'Health probe'
+	})
 	@Get()
 	async checkHealth() {
 		return true;
